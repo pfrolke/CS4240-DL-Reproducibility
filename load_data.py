7e5cce6cf8a2eb6587ca0e963c86a44343e950fc
Paul Frölke <52530667+pfrolke@users.noreply.github.com>
2023-03-28 16:33:51 +0200

    add dataset object

--- load_data.py
+++ load_data.py
@@ -1,5 +1,8 @@
 import numpy as np
 import os
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
 from torchvision.io import read_image
 import torchvision.transforms.functional as fn
 import torchvision.transforms as transforms
@@ -14,10 +17,12 @@ def crop_image(image, left_landmarks, right_landmarks):
     cropped_right = fn.crop(image, tly, tlx, bry - tly, brx - tlx)
     return cropped_left, cropped_right
 
+
 def process_image(image):
     # Perform a histogram equalization and grayscaling on the tensor image
     equalized = fn.equalize(image)
-    return equalized 
+    return equalized
+
 
 def create_dataset_from_mix(path):
     data_set_left = []
@@ -27,24 +32,38 @@ def create_dataset_from_mix(path):
     labels = np.load(os.path.join(path, 'labels.npy'))
     left_landmarks = np.load(os.path.join(path, 'left_landmark_mix.npy'))
     right_landmarks = np.load(os.path.join(path, 'right_landmark_mix.npy'))
-    num_ids = len(os.listdir(path)) - 3 # 3 files are labels, left_landmarks, right_landmarks
-
 
     # load images from path
-    for i in range(num_ids):
-        img_path = os.path.join(path, str(i) + '.png')
+    print("Loading & preprocessing dataset 🚀")
+    for i in tqdm(range(labels.shape[0])):
+        img_path = os.path.join(path, f'{i}.png')
         img = read_image(img_path)
 
-        # Crop the image 
-        cropped_left, cropped_right = crop_image(img, left_landmarks[i], right_landmarks[i])
+        # Crop the image
+        cropped_left, cropped_right = crop_image(
+            img, left_landmarks[i], right_landmarks[i])
 
         # Process the image
         processed_left = process_image(cropped_left)
         processed_right = process_image(cropped_right)
 
         # Append the processed image and label to their respective dataset
-        data_set_left.append((processed_left, labels[i]))
-        data_set_right.append((processed_right, labels[i]))
+        data_set_left.append({'img': processed_left, 'label': labels[i]})
+        data_set_right.append({'img': processed_right, 'label': labels[i]})
 
     return data_set_left, data_set_right
 
+
+class ColumbiaGaze(Dataset):
+    def __init__(self, path):
+        data_set_left, data_set_right = create_dataset_from_mix(path)
+        self.eyes = data_set_left + data_set_right
+
+    def __len__(self):
+        return len(self.eyes)
+
+    def __getitem__(self, idx):
+        return self.eyes[idx]
+
+
+print(len(ColumbiaGaze('data/mix')))
